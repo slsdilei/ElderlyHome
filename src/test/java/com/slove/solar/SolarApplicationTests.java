855f8e5e608b31a60f52369c144b106c3c7a027c
slsdilei <1846408911@qq.com>
2021-02-07 14:45:41 +0800

    first

---
+++ src/test/java/com/slove/solar/SolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slove.solar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
